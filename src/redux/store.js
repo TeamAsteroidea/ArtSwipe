e30a1803a00a22f08735718488d2c26b3e807077
Aidan Fisher <aidanf97@gmail.com>
2023-04-26 15:43:29 -0700

    merge from main

--- src/redux/store.js
+++ src/redux/store.js
@@ -4,10 +4,6 @@ import imageSlice from './imageReducer'
 
 export const store = configureStore({
   reducer: {
-<<<<<<< HEAD
-    message: exampleReducer,
-=======
->>>>>>> 21aa142dc7c47b7e815fa91aafbadf2076838c1a
     images: imageSlice,
   },
 });
