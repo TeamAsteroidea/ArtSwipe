7af9c11a26295378546386d5202845ac03249d6b
Aidan Fisher <aidanf97@gmail.com>
2023-04-25 19:39:13 -0700

    Started using redux

--- src/components/swipestack/index.js
+++ src/components/swipestack/index.js
@@ -1,18 +1,21 @@
 import React, { useState } from 'react';
-// import PropTypes from 'prop-types';
+import PropTypes from 'prop-types';
 // import { store } from '/redux/store';
 import { user, artwork } from './data.js';
 import { View } from 'react-native';
 import { sortArtwork } from './helperFunctions/sortHelperFunctions.js';
 import Display from './Display.jsx';
+import { useSelector } from "react-redux";
 // import styled from 'styled-components/native';
 
+
 function Content ({ navigation }) {
-  const [stack] = useState(sortArtwork(user, artwork));
+  const imageObjs = useSelector((state) => state.images.imagesArrayObj);
+  const [stack] = useState(sortArtwork(user, imageObjs));
 
   return (
     <View>
-        <Display stack={stack} user={user} navigation={navigation}/>
+      <Display stack={stack} user={user} navigation={navigation}/>
     </View>
   );
 }
--- src/scripts/helperFunctions/timeRemaining.js
+++ src/scripts/helperFunctions/timeRemaining.js
@@ -5,9 +5,9 @@ export const timeRemaining = (art) => {
   // subtract todays date from the dateAuctioned
   const timeSpent = today - art.dateAuctioned;
   // if the timeSpent is equal to or less than 0, return 0
-  if (timeSpent < 1) {
-    return 0;
-  }
+  // if (timeSpent < 1) {
+  //   return 0;
+  // }
   // return the time spent from the bid duration
-  return art.bidDuration - timeSpent;
+  return (Math.random() * 1000000) - timeSpent;
 }
