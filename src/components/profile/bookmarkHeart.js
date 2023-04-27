bf6b0cfb98797805914e9fd326858dea51055bff
Mason Lang <masonisblue@gmail.com>
2023-04-27 11:53:10 -0700

    added bookmark placeholder

---
+++ src/components/profile/bookmarkHeart.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import { useState, setState } from 'react';
+import { Text } from 'react-native';
+
+const BookmarkHeart = ({ artID }) => {
+
+  // let initialState = store.user.
+
+  const [ isBookmarked, setIsBookmarked ] = useState(false)
+
+  return (
+    <Text>Place holder</Text>
+  )
+}
+
+export default BookmarkHeart;
