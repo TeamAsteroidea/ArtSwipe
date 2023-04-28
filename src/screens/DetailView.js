f5640d46417473cb955c07589376c40cda2f633b
JakeSifo <bearcoco5@gmail.com>
2023-04-28 16:20:06 -0400

    Merged

--- src/screens/DetailView.js
+++ src/screens/DetailView.js
@@ -15,13 +15,13 @@ import {
 import styled from 'styled-components/native';
 import data from '../../dummyData/artUrlArray.js'
 
-const isCloseToBottom = ({layoutMeasurement, contentOffset, contentSize}) => {
+const isCloseToBottom = ({ layoutMeasurement, contentOffset, contentSize }) => {
   const paddingToBottom = 20;
   return layoutMeasurement.height + contentOffset.y >=
     contentSize.height - paddingToBottom;
 };
 
-const isCloseToTop = ({contentOffset}) => {
+const isCloseToTop = ({ contentOffset }) => {
   const paddingToTop = -100; //negative padding to only do it if overflowing
   return contentOffset.y <=
     paddingToTop;
@@ -41,105 +41,90 @@ const DetailView = ({ navigation, route }) => {
 
 
   return (
-  <SafeWrapper>
-    <ScrollList contentContainerStyle={{ flexGrow: 1}}
-      scrollEventThrottle={8}
-      onScroll={({nativeEvent}) => {
-        if (isCloseToTop(nativeEvent)) {
-          navigation.goBack()
-        }
-      }}
-    >
-      <DetailViewImage
-        source={{
-          uri: route.params.card.image,
-        }}
-      />
     <SafeWrapper>
-      <ScrollList contentContainerStyle={{
-        flexGrow: 1}}
-      scrollEventThrottle={8}
-      onScroll={({ nativeEvent }) => {
-        if (isCloseToTop(nativeEvent)) {
-          navigation.goBack()
-        }
-      }
-    }
+      <ScrollList contentContainerStyle={{ flexGrow: 1 }}
+        scrollEventThrottle={8}
+        onScroll={({ nativeEvent }) => {
+          if (isCloseToTop(nativeEvent)) {
+            navigation.goBack()
+          }
+        }}
       >
-      <DetailViewImageContainer style={{ height: minContainerHeight }}>
-        <DetailViewImage
-          source={{
-            uri: route.params.card.image,
-          }}
-          onLayout={onImageLayout}
-        />
-      </DetailViewImageContainer>
+        <DetailViewImageContainer style={{ height: minContainerHeight }}>
+          <DetailViewImage
+            source={{
+              uri: route.params.card.image,
+            }}
+            onLayout={onImageLayout}
+          />
+        </DetailViewImageContainer>
 
-      <Info>
-        <Title>{route.params.card.name}</Title>
-        <Artist>{route.params.card.artistName}</Artist>
-        <InfoSubtitle>About The Piece</InfoSubtitle>
-        <Text>This piece was made of lorem ipsum in early 19XX during Artist’s chartruse period. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
-        <InfoSubtitle>Materials and Method</InfoSubtitle>
-        <Text>Oil on 20in by 20in canvas over charcoal sketch, really just beat the devil out of those oils. </Text>
-        <InfoSubtitle>About the Artist</InfoSubtitle>
-        <Text>How to use “and” 5 times in a row grammatically: A man owned a store called “This and That” and hired another man to make a sign for it. When the sign was finished, the owner inspected the work. He discovered that the spacing was wrong. So he said to the man, “The space between This and And and And and That is different. Please fix it.”</Text>
-        <InfoSubtitle>About The Piece</InfoSubtitle>
-        <Text>This piece was made of lorem ipsum in early 19XX during Artist’s chartruse period. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
-        <InfoSubtitle>Materials and Method</InfoSubtitle>
-        <Text>Oil on 20in by 20in canvas over charcoal sketch, really just beat the devil out of those oils. </Text>
-        <InfoSubtitle>About the Artist</InfoSubtitle>
-        <Text>How to use “and” 5 times in a row grammatically: A man owned a store called “This and That” and hired another man to make a sign for it. When the sign was finished, the owner inspected the work. He discovered that the spacing was wrong. So he said to the man, “The space between This and And and And and That is different. Please fix it.”</Text>
-      </Info>
-    </ScrollList>
+        <Info>
+          <Title>{route.params.card.name}</Title>
+          <Artist>{route.params.card.artistName}</Artist>
+          <InfoSubtitle>About The Piece</InfoSubtitle>
+          <Text>This piece was made of lorem ipsum in early 19XX during Artist’s chartruse period. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+          <InfoSubtitle>Materials and Method</InfoSubtitle>
+          <Text>Oil on 20in by 20in canvas over charcoal sketch, really just beat the devil out of those oils. </Text>
+          <InfoSubtitle>About the Artist</InfoSubtitle>
+          <Text>How to use “and” 5 times in a row grammatically: A man owned a store called “This and That” and hired another man to make a sign for it. When the sign was finished, the owner inspected the work. He discovered that the spacing was wrong. So he said to the man, “The space between This and And and And and That is different. Please fix it.”</Text>
+          <InfoSubtitle>About The Piece</InfoSubtitle>
+          <Text>This piece was made of lorem ipsum in early 19XX during Artist’s chartruse period. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</Text>
+          <InfoSubtitle>Materials and Method</InfoSubtitle>
+          <Text>Oil on 20in by 20in canvas over charcoal sketch, really just beat the devil out of those oils. </Text>
+          <InfoSubtitle>About the Artist</InfoSubtitle>
+          <Text>How to use “and” 5 times in a row grammatically: A man owned a store called “This and That” and hired another man to make a sign for it. When the sign was finished, the owner inspected the work. He discovered that the spacing was wrong. So he said to the man, “The space between This and And and And and That is different. Please fix it.”</Text>
+        </Info>
+      </ScrollList>
     </SafeWrapper >
   );
 };
 
 DetailView.propTypes = {
   navigation: PropTypes.object.isRequired,
+  route: PropTypes.object.isRequired,
 };
 
 const SafeWrapper = styled.SafeAreaView`
-  flex: 1;
-`
+        flex: 1;
+        `
 
 const ScrollList = styled.ScrollView`
-  flex: 1;
-`
+        flex: 1;
+        `
 
 const DetailViewImageContainer = styled.View`
-  background-color: lightgrey;
-  width: ${Dimensions.get('window').width}px;
-  display: flex;
-`;
+        background-color: lightgrey;
+        width: ${Dimensions.get('window').width}px;
+        display: flex;
+        `;
 
 const DetailViewImage = styled.Image`
-  height: ${Dimensions.get('window').width}px;
-  width: ${Dimensions.get('window').width}px;
-  resize-mode: contain;
-`;
+        height: ${Dimensions.get('window').width}px;
+        width: ${Dimensions.get('window').width}px;
+        resize-mode: contain;
+        `;
 
 const Info = styled.View`
-  padding-top: 30px;
-  padding-left: 20px;
-  padding-right: 20px;
-`
+        padding-top: 30px;
+        padding-left: 20px;
+        padding-right: 20px;
+        `
 
 const Title = styled.Text`
-  font-weight: bold;
-  font-size: 20px;;
-`
+        font-weight: bold;
+        font-size: 20px;;
+        `
 
 const Artist = styled.Text`
-  font-style: italic;
-  font-size: 15px;
-`
+        font-style: italic;
+        font-size: 15px;
+        `
 
 const InfoSubtitle = styled.Text`
-  font-weight: bold;
-  font-size: 15px;
-`
+        font-weight: bold;
+        font-size: 15px;
+        `
 
 
 
