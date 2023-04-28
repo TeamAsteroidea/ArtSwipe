240e8efeb852fc0e6bf6dfe586ff4cc6f0a1d589
Markus Leben <markus_leben@mac.com>
2023-04-28 10:59:03 -0700

    fixed db queries on swipestack

--- src/server/fs-generic.js
+++ src/server/fs-generic.js
@@ -56,5 +56,14 @@ export async function search(collectionName, ...params) {
     return {...doc.data(), _id: _id}
   });
   return data
-
 }
+
+// import { collection, query, where, getDocs } from "firebase/firestore";
+
+// const q = query(collection(db, "cities"), where("capital", "==", true));
+
+// const querySnapshot = await getDocs(q);
+// querySnapshot.forEach((doc) => {
+//   // doc.data() is never undefined for query doc snapshots
+//   console.log(doc.id, " => ", doc.data());
+// });
